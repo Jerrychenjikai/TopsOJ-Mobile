fb0608f9d48ac4af47bdb07e8e81ff700717f8a6
Jerrychenjikai <jerry_chenjikai@hotmail.com>
2025-11-04 10:02:52 -0500

    some changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdkVersion(21)
-        targetSdkVersion(34)
-        versionCode = 3
+        targetSdkVersion(35)
+        versionCode = 4
         versionName = "1.0"
     }
     
