4197eb0b4c4acfb11bd4880c7014fa91351676e0
Jerrychenjikai <jerry_chenjikai@hotmail.com>
2026-06-03 10:17:45 +0800

    publish

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdkVersion(21)
         targetSdkVersion(35)
-        versionCode = 18
-        versionName = "1.0.7"
+        versionCode = 19
+        versionName = "1.0.8"
     }
     
     signingConfigs {
